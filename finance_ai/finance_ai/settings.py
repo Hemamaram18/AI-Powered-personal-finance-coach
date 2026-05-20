129c378c5f2206689b483ab9c2a0f6e312b4d80a
Hema <hemamaram703@gmail.com>
2026-05-20 11:45:15 +0530

    added gmail smtp settings

--- finance_ai/finance_ai/settings.py
+++ finance_ai/finance_ai/settings.py
@@ -179,16 +179,14 @@ REST_FRAMEWORK = {
 
 
 # EMAIL CONFIGURATION
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "hemamaram703@gmail.com"
+EMAIL_HOST_USER = 'hemamaram703@gmail.com'
 
-EMAIL_HOST_PASSWORD = "jkpo vpah vjff gtav"
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = 'qjfj dqdx dbvo uoeh'
