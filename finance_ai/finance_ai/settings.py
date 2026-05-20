ba0d2523030c85edb5d32d6b33761a41d98d506f
Hema <hemamaram703@gmail.com>
2026-05-20 13:17:13 +0530

    fixed email settings

--- finance_ai/finance_ai/settings.py
+++ finance_ai/finance_ai/settings.py
@@ -187,6 +187,6 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_USER = 'hemamaram703@gmail.com'
 
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = 'qjfj dqdx dbvo uoeh'
